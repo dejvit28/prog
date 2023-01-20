d6725fd3935ecc29718eebd60b0ad8676fabcb0d
dejvit28 <122635996+dejvit28@users.noreply.github.com>
2023-01-20 09:54:19 +0100

    Add files via upload

---
+++ vaja3/vaja3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vaja3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stari Donald Tramp ima");
+            Console.WriteLine("IAIAD");
+            Console.WriteLine("Na tej farmi osla ima");
+            Console.WriteLine("IAIAD");
+        }
+    }
+}
